74af6088ebcf96a0e9ff29b0f040651421e6c403
MathMattar <mathmattar@hotmail.com>
2023-01-16 18:04:41 -0300

    Starting from JS

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,8 @@
+const btnTryFree = document.getElementById('btn-try-free');
+const firtName = document.getElementById('firt-name');
+const lastName = document.getElementById('last-name');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const btnSubmit = document.getElementById('submit');
+
 
